ebed2961cbc1998759420c28ffeb1851d9f00adb
Mark W <numedain@gmail.com>
2022-10-31 17:37:25 +0100

    remove errors on load, and show a "this addon is dead an replaced" message in chat

---
+++ deprecated.lua
@@ -0,0 +1,2 @@
+print('The addon TalentTree does not support Dragonflight. You can get rid of this message by removing the addon from your addons folder (or deleting through your addon manager).')
+print('An alternative addon for Dragonflight can be downloaded instead: Talent Tree Viewer')
